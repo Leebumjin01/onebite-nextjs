2019355d2268a3b94df80b1d30b3830ac5455355
lbj <bumttang01@naver.com>
2024-11-12 22:51:30 +0900

    prefetch 예제

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,18 +2,28 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
   const onClickButton = () => {
+    // router.replace("/test"); // 뒤로가기 방지하며 페이지 이동
     router.push("/test");
   };
+
+  // prefetch -> 미리 페이지 번들을 가져옴
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
