56742bbc2a519746aa6f5b2524ec96eb342b9076
lbj <bumttang01@naver.com>
2024-11-15 17:36:13 +0900

    한입북스 UI 구현

---
+++ section02/src/types.ts
@@ -0,0 +1,10 @@
+// interface -> 객체 타입을 정의하기 위한 타입스크립트 문법
+export interface BookData {
+  id: number;
+  title: string;
+  subTitle: string;
+  author: string;
+  publisher: string;
+  description: string;
+  coverImgUrl: string;
+}
