d54fcd61a8502b6074fe688a95f4666821671170
이범진 <lbj0797@dataeum.kr>
2024-11-25 23:26:25 +0900

    스트리밍

---
+++ section06/src/app/(with-searchbar)/search/loading.tsx
@@ -0,0 +1,7 @@
+// 스트리밍 페이지
+// search 경로의 하위 컴포넌트에도 공통으로 적용됨, layout.tsx 랑 비슷...
+// async 키워드가 붙은 컴포넌트에만 적용됨
+// 페이지 컴포넌트에만 적용됨
+export default function Loading() {
+  return <div>Loading...</div>;
+}
